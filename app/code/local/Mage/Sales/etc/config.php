1e1c95720bd0170a8e12887f07f7860e6d097bc4
cMichi <mich@elmueller.net>
2010-09-19 10:24:49 +0200

    config file for separate invoices per store

---
+++ app/code/local/Mage/Sales/etc/config.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Magento on LaTeX Extension
+ *
+ * This is the config file.
+ *
+ * @copyright  Copyright (c) 2010 Michael Mueller (http://micha.elmueller.net)
+ * @license    LGPL
+ */
+
+/*	****** EXAMPLE CONFIG  ******** 
+	The files you specifiy here have to be located at
+		media/lateinvoice/
+*/
+
+$whichTeXFilesToUse = array(
+	1 => 'privat',
+	2 => 'gewerblich'
+);
